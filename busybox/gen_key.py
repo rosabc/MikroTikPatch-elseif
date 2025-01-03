6c39e407c61f00cef374e9f239935179d1363f2e
rosguy <28358617@qq.com>
2025-01-03 13:19:20 +0800

    Create gen_key.py
    Signed-off-by: rosguy <28358617@qq.com>

--- busybox/gen_key.py
+++ busybox/gen_key.py
@@ -13,7 +13,7 @@ def gen_kcdsa_keys():
     return Tools.inttobytes_le(pri_key.scalar, 32), Tools.inttobytes_le(int(pri_key.pubkey.point.x), 32)
 
 
-if name == 'main':
+if __name__=='__main__':
     eddsa_pri_key, eddsa_pub_key = gen_eddsa_keys()
     print("Ed25519 -- ( CUSTOM_NPK_SIGN )")
     print(f"Pri Key: {eddsa_pri_key.hex().upper()}")
